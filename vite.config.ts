0fc79d07c534e572ef0af5e1af8a868ca14fa5c2
Carlos Cardona <ccardonaguillen@gmail.com>
2025-08-10 11:33:09 -0400

    Add tools to manually deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        },
+        plugins: [ghPages()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            },
+        },
     };
 });
